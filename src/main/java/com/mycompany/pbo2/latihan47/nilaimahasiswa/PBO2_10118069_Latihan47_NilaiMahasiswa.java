fffa77f0ad7ca7ecd4064ed951b6a26628b4ffe6
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-11-07 21:27:02 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan47/nilaimahasiswa/PBO2_10118069_Latihan47_NilaiMahasiswa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan47.nilaimahasiswa;
+
+/**
+ *
+ * @author asus
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : IF-2
+ * NIM      : 10118069
+ * Deskripsi Program : Nilai Mahasiswa
+
+ */
+public class PBO2_10118069_Latihan47_NilaiMahasiswa {
+    public static void main(String[] args) {
+        HitungNA na1, na2, na3;
+        
+        na1 = new HitungNA(75,45,34);
+        na1.tampilNa();
+        na2 = new HitungNA(60,80,75);
+        na2.tampilNa();
+        na3 = new HitungNA(30,80,40);
+        na3.tampilNa();
+    }
+}
